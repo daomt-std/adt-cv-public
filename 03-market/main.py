825222fbe3f2781141cb2a60850fc36c0cef3758
Minh Tuan Dao <daomt@students.zcu.cz>
2026-03-05 13:23:09 +0100

    řešený příklad 03

--- 03-market/main.py
+++ 03-market/main.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 import os
 import sys
+import math
 from dataclasses import dataclass
 
 @dataclass
@@ -26,10 +27,24 @@ def load_data(data_path: str, city: str, shop: str, day: str = "1-Mon") -> \
     # pozn. Můžeme použít default dict, nebo použít běžný slovník a při přidání nového záznamu
     # vždy zkontrolovat, zda klíč již existuje, případně inicializovat prázdný list
 
-    city_data: dict[str, list[Record]] = {}
-    # city_data: dict[str, list[Record]] = defaultdict(list)
-
+    # city_data: dict[str, list[Record]] = {}
+    city_data: dict[str, list[Record]] = defaultdict(list)
     print("loading", city)
+
+    path = os.path.join(data_path, city, day, shop + ".txt")
+    with open(path, "r", encoding="utf-8") as f:
+        _ = f.readline()
+        lines = f.readlines()
+        try:
+            for line in lines:
+                line.strip()
+                split_line = line.split(";")
+                time, ckpt, cid, price = split_line
+
+                rec = Record(int(time), int(cid))
+                city_data[ckpt].append(rec)
+        except Exception as e:
+            print(f"Něco se pokazilo: {e}")
 
     return city_data
 
@@ -46,6 +61,11 @@ def get_passed_set(data: dict[str, list[Record]], key_words: list[str]) -> set[i
         set[int]: Funkce vrací množinu identifikačních čísel zákazníků.
     """
     customers: set[int] = set()
+    for key, value in data.items():
+        norm_key = key.split("_")[0]
+        if norm_key in key_words:
+            for rec in value:
+                customers.add(rec.id_cust)
     return customers
 
 def filter_data_time(data: dict[str, list[Record]], cond_time: int) -> dict[str, list[Record]]:
@@ -57,6 +77,11 @@ def filter_data_time(data: dict[str, list[Record]], cond_time: int) -> dict[str,
         dict[str, list[Record]]: vrací data omezená na záznamy s časem menším nebo rovným cond_time.
     """
     ret: dict[str, list[Record]] = defaultdict(list)
+
+    for ckpt, records in data.items():
+        for record in records:
+            if record.time <= cond_time:
+                ret[ckpt].append(record)
     return ret
 
 def get_q_size(data: dict[str, list[Record]], seconds: int) -> int:
@@ -64,10 +89,14 @@ def get_q_size(data: dict[str, list[Record]], seconds: int) -> int:
     Velikost fronty je dána počtem zákazníků, kteří prošli některým z checkpointů
     (vege, frui, meat) a ještě neprošli pokladnou.
     """
-    return 0
+    filtered_data = filter_data_time(data, seconds)
+    before_payment = get_passed_set(filtered_data, ["vege", "frui", "meat"])
+    left = get_passed_set(filtered_data, ["final-crs"])
+    return abs(len(before_payment) - len(left))
 
 def histogram(data: dict[str, list[Record]]) -> None:
-    pass
+    for time in range(0, 24):
+        print(f"{time}:00 - {get_q_size(data, time * 60 * 60)}")
 
 def main(data_path: str) -> None:
     while True:
@@ -80,6 +109,14 @@ def main(data_path: str) -> None:
             shop = "shop_a"
 
         data = load_data(data_path, city, shop)
+        cust_in = get_passed_set(data, ["gate-keeper"])
+        cust_out = get_passed_set(data, ["final-crs"])
+        print(len(cust_in))
+        print(len(cust_out))
+
+        sum_customer = get_q_size(data, 15*60*60)
+        print(sum_customer)
+        histogram(data)
         if data is None:
             continue
 
